9818c41022012c5b57faf848ea7b76a610865da5
mocha8686 <june.mocha8686@gmail.com>
2025-03-25 23:56:54 -0700

    refactor: import alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		format: 'file',
+	},
 	vite: {
 		ssr: {
 			noExternal: ['normalize.css'],
+		},
+		resolve: {
+			alias: { $: path.resolve(__dirname, './src') },
 		},
 	},
 });
