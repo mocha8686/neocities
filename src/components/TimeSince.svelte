512aaf749831a8fc7ae30d8ce8f1c7fe5777ae32
mocha8686 <june.mocha8686@gmail.com>
2025-03-28 11:47:43 -0700

    chore: lint

--- src/components/TimeSince.svelte
+++ src/components/TimeSince.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-	import dayjs from 'dayjs';
-	import relativeTime from 'dayjs/plugin/relativeTime';
-	import type { HTMLTimeAttributes } from 'svelte/elements';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import type { HTMLTimeAttributes } from 'svelte/elements';
 
-	interface Props extends Omit<HTMLTimeAttributes, 'datetime'> {
-		timestamp: string;
-	}
+interface Props extends Omit<HTMLTimeAttributes, 'datetime'> {
+	timestamp: string;
+}
 
-	const { timestamp, ...rest }: Props = $props();
+const { timestamp, ...rest }: Props = $props();
 
-	dayjs.extend(relativeTime);
-	const dateUpdated = dayjs(timestamp);
-	const timeSinceUpdate = dayjs().to(dateUpdated);
+dayjs.extend(relativeTime);
+const dateUpdated = dayjs(timestamp);
+const timeSinceUpdate = dayjs().to(dateUpdated);
 
-	let isHovered = $state(false);
-	let date = $derived(isHovered ? timestamp : timeSinceUpdate);
+let isHovered = $state(false);
+let date = $derived(isHovered ? timestamp : timeSinceUpdate);
 
-	const hover = () => {
-		isHovered = true;
-	};
-	const unhover = () => {
-		isHovered = false;
-	};
+const hover = () => {
+	isHovered = true;
+};
+const unhover = () => {
+	isHovered = false;
+};
 </script>
 
 <time
