2903f9e2fda59dbe2c5691a3b4d1dfe4cccbad7c
mocha8686 <june.mocha8686@gmail.com>
2025-04-12 21:32:36 -0700

    feat: settings

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,20 @@
 import pluginWebc from '@11ty/eleventy-plugin-webc';
 import EleventyVitePlugin from '@11ty/eleventy-plugin-vite';
+import path from 'node:path';
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function config(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('assets')
-
-	eleventyConfig.addPlugin(EleventyVitePlugin);
+	eleventyConfig.addPlugin(EleventyVitePlugin, {
+		viteOptions: {
+			resolve: {
+				alias: {
+					'/node_modules': path.resolve('.', 'node_modules'),
+					'/lib': path.resolve('.', 'lib'),
+					'/assets': path.resolve('.', 'assets'),
+				},
+			},
+		},
+	});
 
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: '_components/**/*.webc',
