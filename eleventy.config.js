b2d2c15870a062d73fa0711042fa0a51bdc9d1dc
mocha8686 <june.mocha8686@gmail.com>
2025-04-13 00:44:48 -0700

    feat: site button

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,29 @@
-import pluginWebc from '@11ty/eleventy-plugin-webc';
 import EleventyVitePlugin from '@11ty/eleventy-plugin-vite';
+import eleventyWebcPlugin from '@11ty/eleventy-plugin-webc';
+import { eleventyImagePlugin } from '@11ty/eleventy-img';
 import path from 'node:path';
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
-export default function config(eleventyConfig) {
+export default function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyWebcPlugin, {
+		components: ['_components/**/*.webc', 'npm:@11ty/eleventy-img/*.webc'],
+	});
+
+	eleventyConfig.addPlugin(eleventyImagePlugin, {
+		formats: ['webp', 'jpeg'],
+		urlPath: '/img/',
+
+		formatFiltering: ['animated'],
+		sharpOptions: {
+			animated: true,
+		},
+
+		defaultAttributes: {
+			loading: 'lazy',
+			decoding: 'async',
+		},
+	});
+
 	eleventyConfig.addPlugin(EleventyVitePlugin, {
 		viteOptions: {
 			resolve: {
@@ -14,9 +34,5 @@ export default function config(eleventyConfig) {
 				},
 			},
 		},
-	});
-
-	eleventyConfig.addPlugin(pluginWebc, {
-		components: '_components/**/*.webc',
 	});
 }
