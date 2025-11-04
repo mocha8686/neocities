324bc97d2672526e21d5ee6906f15c5f8b277035
mocha8686 <june.mocha8686@gmail.com>
2025-11-04 09:21:24 -0800

    fix!: fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
 import alpinejs from '@astrojs/alpinejs';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+	integrations: [alpinejs()],
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.bunny(),
+				name: 'Shippori Mincho',
+				cssVariable: '--font-shippori-mincho',
+				weights: [400],
+				fallbacks: ['MS Mincho', 'Times New Roman', 'serif'],
+			},
+			{
+				provider: fontProviders.bunny(),
+				name: 'Zen Old Mincho',
+				cssVariable: '--font-zen-old-mincho',
+				weights: [400, 700],
+				fallbacks: ['MS Mincho', 'serif'],
+			},
+		],
+	},
 });
