380181beba5b7a7397840b1490cc0fc77abbe655
mocha8686 <june.mocha8686@gmail.com>
2025-11-18 11:41:51 -0800

    feat(cryptopals): layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,6 +26,22 @@ export default defineConfig({
 				weights: [400, 700],
 				fallbacks: ['MS Mincho', 'serif'],
 			},
+
+			// cryptopals
+			{
+				provider: fontProviders.bunny(),
+				name: 'Fira Code',
+				cssVariable: '--font-fira-code',
+				weights: [400],
+				fallbacks: ['monospace'],
+			},
+			{
+				provider: fontProviders.bunny(),
+				name: 'Blinker',
+				cssVariable: '--font-blinker',
+				weights: [300, 700],
+				fallbacks: ['Ubuntu', 'sans-serif'],
+			},
 		],
 	},
 });
